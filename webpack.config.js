784f1e1ed24b6dc0a883633e3578526fb41dcb62
michel <michel.strelow97@gmail.com>
2019-01-17 18:58:35 +0100

    Document webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 /* eslint-disable camelcase */
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlPlugin = require('script-ext-html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const webpack = require('webpack');
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const BrotliPlugin = require('brotli-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const safePostCssParser = require('postcss-safe-parser');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+const BrotliPlugin = require('brotli-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const manifest = require('./src/manifest');
 
+// Find the polyfill chunk
 const polyfillRegex = /(\w|\W)*polyfill\.(\w|\W)*\.js/;
 
+// Compress all text based files
 const compOptions = {
   test: /\.(js|css|html|svg)$/,
+  // When filesize is below a certain size the cost of decompressing
+  // the file is higher than the saving in transfer time
   threshold: 10240,
 };
 
@@ -27,19 +32,30 @@ module.exports = (env, argv) => {
 
   return {
     mode: isProd ? 'production' : 'development',
+    // Create seperate bundles for each part we need to make the app
+    // as compatible with all browsers as possible
     entry: {
+      // RegeneratorRuntime will be needed by all browsers to execute
+      // transpiles async/await
       runtime: './runtime/index.js',
+      // Polyfills are packaged in a seperate bundle because they are only
+      // needed by legacy browsers. They will be loaded with a script tag
+      // with the 'nomodule' attribute, so modern browsers won't download
+      // the bundle
       polyfill: './polyfill/index.js',
+      // Main bundle
       main: './src/index.js',
     },
     devtool: isDev ? 'source-map' : false,
     devServer: {
       contentBase: './dist',
       port: 8080,
+      // Logging will be done by FriendlyErrorsPlugin
       quiet: true,
     },
     module: {
       rules: [{
+        // Show eslint warnings in build and in browser console
         enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
@@ -61,10 +77,12 @@ module.exports = (env, argv) => {
                 ],
               },
             }]],
+            // Support for dynamic imports
             plugins: ['@babel/plugin-syntax-dynamic-import'],
           },
         }],
       }, {
+        // Include and compile and prefix .sass files
         test: /\.sass$/,
         use: [
           isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
@@ -74,6 +92,7 @@ module.exports = (env, argv) => {
           'sass-loader',
         ],
       }, {
+        // Include and prefix .css files
         test: /\.css$/,
         use: [
           isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
@@ -82,6 +101,7 @@ module.exports = (env, argv) => {
           'postcss-loader',
         ],
       }, {
+        // Load fonts
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         exclude: /node_modules/,
         loader: 'file-loader',
@@ -91,8 +111,11 @@ module.exports = (env, argv) => {
       }],
     },
     plugins: [
+      // More readable webpack output on dev builds
       new FriendlyErrorsPlugin(),
+      // Remove old build folder before build
       new CleanWebpackPlugin(['dist']),
+      // Use template html and minify output
       new HtmlWebpackPlugin({
         template: 'public/index.html',
         minify: isProd
@@ -110,18 +133,18 @@ module.exports = (env, argv) => {
           }
           : false,
       }),
+      // Set 'nomodule' attribute on polyfill script tag,
+      // so newer browsers, which do not need the polyfills,
+      // won't download the script
       new ScriptExtHtmlPlugin({
         custom: {
           test: polyfillRegex,
           attribute: 'nomodule',
         },
       }),
-      new WebpackPwaManifest({
-        name: 'Minimal Bundle',
-        short_name: 'MiniBundle',
-        description: 'This is a sample app for a minimal bundle size.',
-        background_color: '#000000',
-      }),
+      // Generate manifest file
+      new WebpackPwaManifest(manifest),
+      // Extract css files into one css bundle
       new MiniCssExtractPlugin({
         filename: isDev
           ? 'styles/[name].bundle.css'
@@ -130,9 +153,12 @@ module.exports = (env, argv) => {
           ? 'styles/[name].chunk.css'
           : 'styles/[name].[contenthash].chunk.css',
       }),
+      // Compress text files
       new BrotliPlugin(compOptions),
       new CompressionPlugin(compOptions),
       ...(isDev ? [new webpack.HotModuleReplacementPlugin()] : []),
+      // Get an overview of all scripts and libraries
+      // and their sizes in your bundle
       new BundleAnalyzerPlugin({
         analyzerMode: isDev ? 'server' : 'static',
         analyzerPort: 8081,
@@ -142,10 +168,14 @@ module.exports = (env, argv) => {
     ],
     optimization: {
       minimize: isProd,
+      // Pull libraries out into seperate chunks for better caching
       splitChunks: {
+        // Do not split polyfill and regeneratorRuntime libraries
+        // out of scripts, since they will likely not change
         chunks: ({ name }) => name !== 'polyfill' && name !== 'runtime',
       },
       minimizer: [
+        // Minify js files
         new TerserPlugin({
           terserOptions: {
             parse: {
@@ -171,6 +201,7 @@ module.exports = (env, argv) => {
           cache: true,
           sourceMap: isDev,
         }),
+        // Minify css files
         new OptimizeCSSAssetsPlugin({
           cssProcessorOptions: {
             parser: safePostCssParser,
@@ -181,7 +212,9 @@ module.exports = (env, argv) => {
     },
     output: {
       filename: isDev
+        // Don't hash file in development mode for better build performance
         ? 'js/[name].bundle.js'
+        // Hash files in production for better caching
         : 'js/[name].[chunkhash].bundle.js',
       chunkFilename: isDev
         ? 'js/[name].chunk.js'
